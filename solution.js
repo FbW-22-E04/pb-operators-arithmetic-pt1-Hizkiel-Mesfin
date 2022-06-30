96e0db9110bfec04f3779b0657eec191379fd835
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-06-30 14:45:21 +0200

    done with the math

--- solution.js
+++ solution.js
@@ -1,1 +1,88 @@
 // write your code in this file
+//#1
+const ticketPrice = 15;
+const totalAmount = 6450;
+
+console.log(`Q1: ${totalAmount / ticketPrice} are sold`);
+
+//#2
+const numMonthYear = 12;
+const numWeekMonth = 4;
+const weekSalary = 500;
+
+console.log(
+  `Q2: Sylvia makes ${numMonthYear * numWeekMonth * weekSalary} a year`
+);
+
+//#3
+const num = 17 / 30;
+
+console.log(`Q3: ${num * 100}`);
+
+//#4
+
+const side = 4.75;
+console.log(`Q4: The perimeter of the square is ${4 * side}`);
+
+//#5
+
+const triSideOne = 5;
+const triSideTwo = 6;
+const triSideThree = 7;
+
+console.log(
+  `Q5: The perimeter of the triangle is ${
+    triSideOne + triSideTwo + triSideThree
+  }`
+);
+
+//#6
+
+const squareSide = 5;
+console.log(`Q6: The area of the sqaure is ${squareSide ** 2}`);
+
+//#7
+
+const tri2SideOne = 5;
+const tri2SideTwo = 3;
+const tri2SideThree = 4;
+
+console.log(
+  `Q7: The area of the triangle is ${0.5 * tri2SideTwo * tri2SideThree}`
+);
+
+//#8
+
+const cubeSide = 9;
+
+console.log(`Q8: The volume of the cube is ${cubeSide ** 3}`);
+
+//#9
+
+const billOne = 22.35;
+const billTwo = 26.67;
+const billThree = 35.92;
+
+console.log(
+  `Q9: The first bill is ${billOne * 0.1 + billOne}, the second bill is ${
+    billTwo * 0.15 + billTwo
+  } and finally the third bill is ${billThree * 0.2 + billThree} `
+);
+
+//#10
+
+console.log(
+  `Q10: Noemy's average hours worked per day is ${
+    (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10
+  }`
+);
+
+//#11
+
+console.log(
+  `Q11: Score in the sixth text is ${85 * 6 - (75 + 70 + 85 + 90 + 100)}`
+);
+
+//#12
+
+console.log(`Q12: James needs a minimum of ${100 - 4}% to get an 80% average `);
